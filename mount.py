7a3cb33e79532ebed48932e57783d3116879851c
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-12-15 21:08:58 +0000

    Add files via upload

--- mount.py
+++ mount.py
@@ -3,7 +3,7 @@ i=input()
 f1=open(i,"r")
 ff=f1.read()
 f1.close()
-index=0;
+index=0
 ii=i.replace(".txt",".out")
 fff=ff.split(",")
 f2=open(ii,"w")
@@ -16,7 +16,6 @@ for f in fff:
     g=f1.read()
     g=g.replace("|","\xff")
     f1.close()
-    g="|"+f+"|"+g
+    g=f+"|"+g+"|"
     f2.write(g)
-f2.write("|")
 f2.close()
